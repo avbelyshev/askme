c31338b8f1f72a9562aa54ac3436f6d2380ba077
avbelyshev <avbelishev@gmail.com>
2017-11-10 01:31:12 +0300

    Add SessionsController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,18 @@
 class UsersController < ApplicationController
   before_action :load_user, except: [:index, :create, :new]
+  before_action :authorize_user, except: [:index, :new, :create, :show]
 
   def index
     @users = User.all
   end
 
   def new
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
     @user = User.new
   end
 
   def create
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
     @user = User.new(user_params)
 
     if @user.save
@@ -38,6 +41,10 @@ class UsersController < ApplicationController
 end
 
 private
+
+def authorize_user
+  reject_user unless @user == current_user
+end
 
 def load_user
   @user ||= User.find params[:id]
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, except: [:destroy]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :questions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get 'sign_up' => 'users#new'
+  get 'log_out' => 'sessions#destroy'
+  get 'log_in' => 'sessions#new'
 end
