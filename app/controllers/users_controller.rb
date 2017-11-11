2285674adaff48d35c37bec0de0c9dc872eb2dd1
avbelyshev <avbelishev@gmail.com>
2017-11-11 23:16:16 +0300

    Question counters for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,6 +37,10 @@ class UsersController < ApplicationController
     @questions = @user.questions.order(created_at: :desc)
 
     @new_question = @user.questions.build
+
+    @questions_count = @questions.count
+    @answers_count = @questions.where.not(answer: nil).count
+    @unanswered_count = @questions_count - @answers_count
   end
 end
 
