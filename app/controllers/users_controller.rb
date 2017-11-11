ad080cc59bb66f95cf45309a5f180ce6d8ea3848
avbelyshev <avbelishev@gmail.com>
2017-11-12 00:37:56 +0300

    log in after sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
+      @current_user = @user.id
       redirect_to root_url, notice: 'Пользователь успешно зарегестрирован!'
     else
       render 'new'
