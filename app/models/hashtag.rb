e239d0e758a5508841ad0d483a53bfed98b7fef8
avbelyshev <avbelishev@gmail.com>
2018-01-21 02:26:17 +0300

    Models for hashtags

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,2 +1,5 @@
 class Hashtag < ApplicationRecord
+  has_and_belongs_to_many :questions
+
+  validates :name, uniqueness: true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,11 +1,23 @@
 class Question < ApplicationRecord
   belongs_to :user
   has_one :author, class_name: 'User'
+  has_and_belongs_to_many :hashtags
 
   validates :user, :text, presence: true
   validates :text, length: { maximum: 255 }
 
+  before_save :add_hashtags
+
   def question_author
     User.find(author_id) unless author_id == nil
+  end
+
+  def add_hashtags
+    self.hashtags.clear
+    tags_list = (text + answer).scan(/#[\p{L}0-9_]/).map(&:downcase).uniq
+    tags_list.each do |tag|
+      hashtag = Hashtag.find_or_create_by(name: tag)
+      self.hashtags << hashtag
+    end
   end
 end
