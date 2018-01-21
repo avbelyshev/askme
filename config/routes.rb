ea03524758d2c094250d9f8c65c344f607ee9d65
avbelyshev <avbelishev@gmail.com>
2018-01-21 19:11:52 +0300

    VC for hashtags

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :users #, except: [:destroy]
   resource :session, only: [:new, :create, :destroy]
   resources :questions, except: [:show, :new, :index]
+  resources :hashtags, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
